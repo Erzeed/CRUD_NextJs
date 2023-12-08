c5246c144a8df7473c07388925541bff271f0305
Feizal Reza <feizal.reza@students.amikom.ac.id>
2023-12-08 22:39:19 +0700

    add navbar

--- pages/products/list_products.tsx
+++ pages/products/list_products.tsx
@@ -1,7 +1,11 @@
+import Navbar from "@/components/Navbar";
+
 export default function ListProducts() {
   return (
-    <div className="container__listproduk">
-      <div className="listproduk__header"></div>
+    <div className="container__listproduk antialiased">
+      <div className="listproduk__header px-5 flex justify-between items-center w-full h-[60px] border-b">
+        <Navbar />
+      </div>
       <div className="list_produk__main"></div>
     </div>
   );
